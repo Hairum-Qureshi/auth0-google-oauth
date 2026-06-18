339b2a696a6e8bec18151b8bf4569e1f025de51b
Hairum Qureshi <hqureshi@udel.edu>
2026-06-18 07:23:08 -0400

    updated validate method so it handles getting the current user's data

--- apps/backend/src/auth/jwt-strategy.ts
+++ apps/backend/src/auth/jwt-strategy.ts
@@ -4,10 +4,15 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { passportJwtSecret } from 'jwks-rsa';
 import { ConfigService } from '@nestjs/config';
 import { Payload } from 'src/types';
+import { firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {
     super({
       secretOrKeyProvider: passportJwtSecret({
         cache: true,
@@ -19,10 +24,44 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       audience: configService.get<string>('AUTH0_AUDIENCE'),
       issuer: configService.get<string>('AUTH0_ISSUER'),
       algorithms: ['RS256'],
+      passReqToCallback: true,
     });
   }
 
-  validate(payload: Payload) {
-    return payload;
+  async validate(req: Request, payload: Payload) {
+    const bearerToken = req.headers['authorization'];
+    const userProfileURL = payload.aud[1];
+
+    const userProfileData = await firstValueFrom(
+      this.httpService.post(userProfileURL, null, {
+        headers: {
+          Authorization: `${bearerToken}`,
+        },
+      }),
+    );
+
+    const {
+      given_name,
+      family_name,
+      nickname,
+      name,
+      picture,
+      updated_at,
+      email,
+      email_verified,
+      sub,
+    } = userProfileData.data;
+
+    return {
+      firstName: given_name,
+      lastName: family_name,
+      nickname,
+      fullName: name,
+      profilePicture: picture,
+      updatedAt: updated_at,
+      email,
+      emailVerified: email_verified,
+      userID: sub.replace('google-oauth2|', ''),
+    };
   }
 }
