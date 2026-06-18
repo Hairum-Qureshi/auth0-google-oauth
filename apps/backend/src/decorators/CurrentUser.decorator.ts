d88e2fc456604bb1d8ca451b9ca144ec43b040f8
Hairum Qureshi <hqureshi@udel.edu>
2026-06-18 07:23:32 -0400

    modified custom current-user decorator so it simply just returns the user object and no longer handles fetching the user data

--- apps/backend/src/decorators/CurrentUser.decorator.ts
+++ apps/backend/src/decorators/CurrentUser.decorator.ts
@@ -1,40 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import axios from 'axios';
 
 export const CurrentUser = createParamDecorator(
-  async (_: unknown, ctx: ExecutionContext) => {
-    const request: any = ctx.switchToHttp().getRequest();
-    const bearerToken = request.headers['authorization'] || '';
-    const payload = request.user;
-    const userProfileURL = payload.aud[1];
-    const userProfileData = await axios.get(userProfileURL, {
-      headers: {
-        Authorization: `${bearerToken}`,
-      },
-    });
-
-    const {
-      given_name,
-      family_name,
-      nickname,
-      name,
-      picture,
-      updated_at,
-      email,
-      email_verified,
-      sub,
-    } = userProfileData.data;
-
-    return {
-      firstName: given_name,
-      lastName: family_name,
-      nickname,
-      fullName: name,
-      profilePicture: picture,
-      updatedAt: updated_at,
-      email,
-      emailVerified: email_verified,
-      userID: sub.replace('google-oauth2|', ''),
-    };
+  (_: unknown, ctx: ExecutionContext) => {
+    return ctx.switchToHttp().getRequest().user;
   },
 );
