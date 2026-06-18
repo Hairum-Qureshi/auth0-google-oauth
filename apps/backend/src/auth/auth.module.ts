0328190ab2c40a6ba1b0f31f6c905a8c2ccfbbc3
Hairum Qureshi <hqureshi@udel.edu>
2026-06-18 06:38:33 -0400

    configured JWT and Passport authentication

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -2,10 +2,30 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { HttpModule } from '@nestjs/axios';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt-strategy';
 
 @Module({
-  imports: [HttpModule],
-  providers: [AuthService],
+  imports: [
+    HttpModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: Number(config.get<string>('JWT_EXPIRES')),
+          },
+        };
+      },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [PassportModule, JwtStrategy],
 })
 export class AuthModule {}
